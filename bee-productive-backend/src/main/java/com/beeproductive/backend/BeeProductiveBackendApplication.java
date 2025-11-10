7c00146ff90a7817e5e95d9a7dbd2372c2df5f86
Gherman Larisa <gherman.larisa3@gmail.com>
2025-11-10 13:32:54 +0200

    springboot initialised

---
+++ bee-productive-backend/src/main/java/com/beeproductive/backend/BeeProductiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.beeproductive.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeProductiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeProductiveBackendApplication.class, args);
+	}
+
+}
